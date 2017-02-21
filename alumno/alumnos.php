a37697bdf8be83cf530801c4230a362ced07f278
Uriel A Camacho Hernandez <blizzcon2005@gmail.com>
2017-02-20 19:02:07 -0600

    Conexion correcta con MariaDB, falta asignar correctamente los campos al array para codificarlo en formato JSON posteriormente

--- alumno/alumnos.php
+++ alumno/alumnos.php
@@ -1,4 +1,30 @@
 <?php
+	$server = "localhost";
+	$user = "root";
+	$pswd = "";
+	$database = "PDistribuida";
+	//Creando la conexion
+	$conexion = mysqli_connect($server,$user, $pswd, $database) or die();
+	function buscaAlumnos(){
+		//echo "HOLA";
+		$sql = "SELECT * FROM alumnos";
+		//mysqli_set_charset($conexion, "utf8");
+		if(!$result = mysqli_query($conexion, $sql)) die ();
+		echo "HOLA";
+		$alumnos = array();
+		var_dump($fila = mysqli_fetch_array($result));
+		while($fila = mysqli_fetch_array($result)){
+			$mat = $fila['matricula'];
+			echo $mat;
+			$nombre = $fila['nombre'];
+			$edad = $fila['edad'];
+
+			$alumnos[] = array('matricula'=> $mat, 'nombre'=> $nombre, 'edad'=> $edad);
+			var_dump($alumnos);
+		}
+		return alumnos;
+	}
+	
 	function mostrarCursos(){
 		$cursos = array('AngularJS','Base de Datos','POO','Ensamblador');
 		return $cursos;
@@ -12,7 +38,10 @@
 		$resultado = mostrarCursos();
 	}else if ($_GET['solicitud'] == 'listas'){
 		$resultado = mostrarAlumnos();
-		}else{
+		}else if($_GET['solicitud'] == 'alumnos'){
+			//echo "listo";
+			$resultado = buscaAlumnos();
+			}else{
 			header('http/1.1 405 METHOD NOT ALLOWED');
 			exit;
 	}
